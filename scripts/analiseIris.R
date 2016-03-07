f567658a6c9e88e2e78d1ef2f6a3ff7b149ffbfb
Fabricio Barth <fbarth@br.ibm.com>
2016-03-07 20:53:32 -0300

    Aula 05

--- scripts/analiseIris.R
+++ scripts/analiseIris.R
@@ -16,5 +16,31 @@ boxplot(iris$Petal.Width,
         col="cyan",
         main="Largura das pétalas")
 
+boxplot(iris[,1:4], col='cyan')
+
+plot(iris$Sepal.Length, 
+     iris$Sepal.Width, 
+     pch=19, col='red')
+
+plot(iris$Petal.Length, 
+     iris$Petal.Width, 
+     pch=19, col='red')
+
+plot(iris[, 1:4], 
+     col='red',
+     pch=19)
+
+cor(iris$Sepal.Length, iris$Sepal.Width)
+cor(iris$Petal.Length, iris$Petal.Width)
+# cor implementa o calculo de correlacao de Pearson.
+# cor => [-1, +1]
+# cor = -1 => correlacao forte negativa.
+# cor = +1 => correlacao forte positiva.
+# cor = 0 => sem correlacao
+
+plot(iris$Petal.Length, 
+     iris$Petal.Width, 
+     pch=19, col=iris$Species)
+
 
 
